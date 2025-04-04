5ef9992bc54cb364662338e93c411ac89b32e048
caiodeveloper <caiquemacacos@gmail.com>
2025-04-04 18:58:16 -0300

    Sistema login/registro c/ MYSQL.

--- src/main/java/org/caique/mcgames/Projeto.java
+++ src/main/java/org/caique/mcgames/Projeto.java
@@ -1,17 +1,40 @@
 package org.caique.mcgames;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.caique.mcgames.commands.LoginCommand;
+import org.caique.mcgames.commands.RegisterCommand;
+import org.caique.mcgames.database.DatabaseManager;
+import org.caique.mcgames.events.PlayerListener;
 
 public final class Projeto extends JavaPlugin {
 
+    private DatabaseManager databaseManager;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        save/DefaultConfig(); // Gera config.yml se não existir
+        databaseManager = new DatabaseManager(this);
+        databaseManager.connect();
 
+        // Registrar comandos
+        getCommand("login").setExecutor(new LoginCommand(this));
+        getCommand("register").setExecutor(new RegisterCommand(this));
+
+        // Registrar eventos
+        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+
+        getLogger().info("Plugin iniciado com sucesso!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (databaseManager != null) {
+            databaseManager.disconnect();
+        }
+        getLogger().info("Plugin desativado.");
+    }
+
+    public DatabaseManager getDatabaseManager() {
+        return databaseManager;
     }
 }
